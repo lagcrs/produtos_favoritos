f0c41db0c94bcb29b84ee8262e0fa2f4e09ff916
Larissa Silva <larissa.gcrs@gmail.com>
2020-12-11 23:52:55 -0300

    config database

---
+++ src/database.js
@@ -0,0 +1,8 @@
+//database config and connection file
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://user:iEarwAbh84Oz3K0k@cluster0.c7czm.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+})
+
+module.exports = mongoose;
