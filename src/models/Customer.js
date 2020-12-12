0ecadb8e5e74777a2ead3b54c2d78bd49e819899
Larissa Silva <larissa.gcrs@gmail.com>
2020-12-11 23:53:22 -0300

    add model customer

---
+++ src/models/Customer.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const CustomerSchema = new Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        require: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        require: true,
+        select: false
+    }
+}, {
+    timestamps: true
+});
+
+// creating password hash before save new customer 
+CustomerSchema.pre('save', async function(next){
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash;
+    next()
+});
+
+module.exports = model('Customer', CustomerSchema);
