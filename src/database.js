a65f216af5f1fd085bca194b1d2e6226ca49aba7
Larissa Silva <larissa.gcrs@gmail.com>
2020-12-13 14:42:00 -0300

    more database config

--- src/database.js
+++ src/database.js
@@ -2,7 +2,10 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://user:iEarwAbh84Oz3K0k@cluster0.c7czm.mongodb.net/<dbname>?retryWrites=true&w=majority', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
 })
 
 module.exports = mongoose;
