7bd7eb5d276b1d9ec3099f93389d2254ca3f3245
Larissa Silva <larissa.gcrs@gmail.com>
2020-12-13 23:22:29 -0300

    fix minor typos

--- src/database.js
+++ src/database.js
@@ -1,9 +1,6 @@
 //database config and connection file
 const mongoose = require('mongoose');
 
-// require dotenv to use environment variables
-require('dotenv').config();
-
 mongoose.connect(`mongodb+srv://user:${process.env.DB_PASS}@cluster0.c7czm.mongodb.net/<dbname>?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
