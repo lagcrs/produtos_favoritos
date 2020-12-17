ede56054f336126fe4c77ce8f99900d77de68be9
Larissa Silva <larissa.gcrs@gmail.com>
2020-12-16 23:36:16 -0300

    add more conditions for list favorite products

--- src/controllers/FavoriteProductsController.js
+++ src/controllers/FavoriteProductsController.js
@@ -4,42 +4,53 @@ const Customer = require('../models/Customer');
 
 module.exports = {
     async getList(req, res){
+        // get user logged id
         const userId = req.userId;
         try {
+            // find user on database
             const user = await Customer.findById(userId);
 
+            // return list of favorite products
             return res.status(200).send({ listFavoriteProducts: user.favoriteProducts })
 
         }catch(err){
+            // return message error if falid to get list
             return res.status(500).send({ error: 'Falid to get list.' })
         }
     },
 
     async addProduct(req, res) {
+        // get productId and user logged id
         const { productId } = req.params;
+        const userId = req.userId;
 
         try {
+            // with axios, get from luizalabs api the product by id
             const response = await axios.get(`http://challenge-api.luizalabs.com/api/product/${productId}/`);
 
+            // save response from luizalabs api
             const getProduct = response.data;
-    
-            // if(!getProduct) {
-            //     return res.status(400).send({ error: 'Product not found.' })
-            // }
+            
+            // find user on database
+            const user = Customer.findById(req.userId)
 
-            if(await Customer.find({ 'favoriteProducts.id': getProduct.id }).where('id').equals(req.userId)) {
+            // verify if already exists favorite product on user list, search on database by id of product
+            if(await user.findOne({ 'favoriteProducts': {
+                $elemMatch: { id: getProduct.id }
+            } })) {
                 return res.status(400).send({ error: 'Favorite product already exists in list.' })
             }
-            
-            const userId = req.userId;
+
+            // save getProduct on user list
             await Customer.findByIdAndUpdate(userId, {
                 $push: { favoriteProducts: getProduct }
             }).exec();
             
-
+            // return success massage
             return res.status(201).send({ message: 'Product add successfully!' })
 
         } catch(err) {
+            // return message error if falid to add product
             return res.status(500).send({ error: 'Falid to add product' })
         }
     }
